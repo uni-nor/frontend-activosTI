7099755200b9ecc7d4467beb79ebd78cb22bf6eb
norqf <norqf@hotmail.com>
2024-06-06 20:15:24 -0400

    Eliminacion de menus y componentes que no corresponden al proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,50 +14,20 @@ export const routes: Routes = [
       title: 'Home'
     },
     children: [
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./views/dashboard/routes').then((m) => m.routes)
-      },
+      // {
+      //   path: 'dashboard',
+      //   loadChildren: () => import('./views/dashboard/routes').then((m) => m.routes)
+      // },
       {
         path: 'activosti',
         loadChildren: () => import('./views/activosTI/routes').then((m) => m.routes)
       },
-      {
-        path: 'theme',
-        loadChildren: () => import('./views/theme/routes').then((m) => m.routes)
-      },
-      {
-        path: 'base',
-        loadChildren: () => import('./views/base/routes').then((m) => m.routes)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'forms',
-        loadChildren: () => import('./views/forms/routes').then((m) => m.routes)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/icons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'notifications',
-        loadChildren: () => import('./views/notifications/routes').then((m) => m.routes)
-      },
-      {
-        path: 'widgets',
-        loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
-      },
-      {
-        path: 'charts',
-        loadChildren: () => import('./views/charts/routes').then((m) => m.routes)
-      },
-      {
-        path: 'pages',
-        loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
-      }
+      
+      // {
+      //   path: 'widgets',
+      //   loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
+      // },
+     
     ]
   },
   {
